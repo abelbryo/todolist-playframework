2aaf261e800b2254ad1a280885226cbdd7bbc03d
Abel Terefe <abelbryo@gmail.com>
2012-12-20 09:28:51 +0200

    Initial init -  playframework

---
+++ app/models/Task.java
@@ -0,0 +1,39 @@
+package models;
+
+import java.util.*;
+
+import play.db.ebean.*;
+import play.data.validation.Constraints.*;
+import javax.persistence.*;
+
+@Entity
+public class Task extends Model {
+    @Id
+    public Long id; 
+
+    @Required
+    public String label; 
+
+    public static List<Task> all() {
+        return find.all();
+    }
+
+    public static void create(Task task){
+        task.save();
+    }
+
+    public static void delete(Long id){
+        find.ref(id).delete();
+    }
+
+    public static void edit(Long id){
+        // to do
+    }
+
+    public static Finder<Long, Task> find = new  Finder(Long.class, Task.class);
+
+
+
+
+
+}
